12c6058b975904c251ee2c031d653b57ab8a55de
Kasun-Madusanka-Bandara <kasunmadhusanka44@outlook.com>
2024-10-11 14:05:30 +0530

    first java program

---
+++ src/first_program.java
@@ -0,0 +1,16 @@
+public class first_program {
+    public static void main(String[]args){
+        System.out.println("I love pizza !");
+        System.out.print("It's really good!");   // sout + tab
+        System.out.println();
+
+        // this is one line comment
+
+        /* this
+        is
+        the
+        multi line comment
+         */
+
+    }
+}
