9fddbf5e3220f1c1579d9baa7fd9b9dad8ee8c1f
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-11 19:06:03 +0530

    Testing push req

--- src/gui_application.java
+++ src/gui_application.java
@@ -1,5 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class gui_application {
     public static void main(String[]args){
+
+        String name = JOptionPane.showInputDialog("Enter Your Name: ");
+        JOptionPane.showMessageDialog(null, "Hello "+name+"!");
+
+        int age = JOptionPane.showInputDialog("Enter your Age: ");
 
 
     }
