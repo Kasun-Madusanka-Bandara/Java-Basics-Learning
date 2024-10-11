daddc0ea94a51f90116ffba6ec410d517c6280bc
K.P.G. Kasun Madusanka Bandara <kasunmadhusanka44@outlook.com>
2024-10-11 16:06:24 +0530

    expression

---
+++ src/expression.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class expression {
+    public static void main(String[]args){
+
+        // Expression = Operands and Operators
+        // Operands = Values, Variables , Numbers , Quantity
+        // Operators = + - * / %
+
+
+
+
+
+    }
+}
