a15cebed4fdd15905818c6d8694702e4893824dc
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-11 20:21:11 +0530

    simple GUI application to get user input

--- src/gui_application.java
+++ src/gui_application.java
@@ -4,10 +4,13 @@ public class gui_application {
     public static void main(String[]args){
 
         String name = JOptionPane.showInputDialog("Enter Your Name: ");
-        JOptionPane.showMessageDialog(null, "Hello "+name+"!");
+        JOptionPane.showMessageDialog(null, "Hello "+name+" !");
 
-        int age = JOptionPane.showInputDialog("Enter your Age: ");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your Age: "));
+        JOptionPane.showMessageDialog(null, "Your are "+age+" years old !");
 
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your Height: "));
+        JOptionPane.showMessageDialog(null, "Your are "+ height +" cm tall");
 
     }
 }
