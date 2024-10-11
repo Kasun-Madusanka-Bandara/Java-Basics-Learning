ada68aea7cce6f93142c696e4ff20caca21d3eea
K.P.G. Kasun Madusanka Bandara <kasunmadhusanka44@outlook.com>
2024-10-11 16:29:00 +0530

    Expressions

--- src/expression.java
+++ src/expression.java
@@ -7,9 +7,39 @@ public class expression {
         // Operands = Values, Variables , Numbers , Quantity
         // Operators = + - * / %
 
+        int value = 0;
+        int price = 20 ;
+
+        value = price + 10;
+        System.out.println("price + 10 = "+value);
+
+        value = price - 10;
+        System.out.println("price - 10 = "+value);
+
+        value = price * 2;
+        System.out.println("price * 2 = "+value);
+
+        value = price % 3;
+        System.out.println("price % 3 = "+value);
 
 
+        price++; // Increment one value
+        System.out.println("price-- = "+price);
 
+        price = 20;
+
+        price--; // Discernment one value
+        System.out.println("price-- = "+price);
+
+        price = 20;
+
+        value = price / 3;
+        System.out.println("price / 3 = "+value);
+
+
+        double gpa = 10 ;
+        gpa = gpa / 3;   // (double) friends / 3
+        System.out.println("gpa / 3 = "+gpa);
 
     }
 }
