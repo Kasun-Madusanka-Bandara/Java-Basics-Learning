c7f0a94af8432222894dad70f0e3f8d072ab7dad
K.P.G. Kasun Madusanka Bandara <kasunmadhusanka44@outlook.com>
2024-10-11 14:42:38 +0530

    Variables in java

---
+++ src/Variables.java
@@ -0,0 +1,33 @@
+public class Variables {
+    public static void main(String[]args){
+
+        int x ; // declaration
+        x = 20; // Assignment
+
+        int y = 10; // initialization
+
+        System.out.println(x);
+        System.out.println(y);
+
+        int age = 21;
+
+        System.out.println("My age is : "+age);
+
+        long a = 2020210888200L; // Need to add L for at the end
+
+        byte b = 100;  // store short values
+
+        float c = 0.3f;   // Need to add f
+
+        double d = 22.99;
+
+        boolean e = true; // or false
+
+        char word = 'f';
+
+        String name = "Kasun Madusanka Bandara";
+
+        System.out.println("Hello "+name+"!");
+
+    }
+}
